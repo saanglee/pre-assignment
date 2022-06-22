b28835d63ab17cf1432d2608ea9f29d5ed861b51
sangji <sangllee20@gmail.com>
2022-06-22 14:19:53 +0900

    Add icons

--- src/assets/index.js
+++ src/assets/index.js
@@ -4,3 +4,6 @@ export { ReactComponent as DotsIcon } from './dots.svg';
 export { ReactComponent as LikeIcon } from './like.svg';
 export { ReactComponent as ShareIcon } from './share.svg';
 export { ReactComponent as SmileIcon } from './smile.svg';
+export { ReactComponent as HomeIcon } from './home.svg';
+export { ReactComponent as CompassIcon } from './compass.svg';
+export { ReactComponent as SearchIcon } from './search.svg';
