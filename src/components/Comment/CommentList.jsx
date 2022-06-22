f0c5be3ad0eb58e0f561d729e26424a7a663e4d9
sangji <sangllee20@gmail.com>
2022-06-22 14:22:57 +0900

    Feature: remove comment

--- src/components/Comment/CommentList.jsx
+++ src/components/Comment/CommentList.jsx
@@ -1,22 +1,22 @@
 import React from 'react';
-
 import styles from './comment.module.scss';
 
 const CommentList = ({ commentList, onRemove }) => {
-  // FIXME: 삭제가 안됨
-  const handleClick = () => {
-    if (window.confirm('정말 삭제하시겠습니까?')) {
-      onRemove(id);
-    }
-  };
-
   return (
     <div className={styles.CommentList}>
       <p>댓글 {commentList.length}개</p>
       {commentList.map((item) => (
         <ul key={item.id}>
           <li>{item.comment}</li>
-          <button onClick={handleClick}>삭제</button>
+          <button
+            onClick={() => {
+              if (window.confirm('댓글을 삭제하시겠습니까?')) {
+                onRemove(item.id);
+              }
+            }}
+          >
+            삭제
+          </button>
         </ul>
       ))}
     </div>
